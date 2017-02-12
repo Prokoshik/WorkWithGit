669d15afdf78643fb9d4dbf59f4e0b23da876e68
vika.prokofyeva <vika.prokofyeva@gmail.com>
2017-02-12 22:49:32 +0300

    new window

--- WorkWithGit/Form1.cs
+++ WorkWithGit/Form1.cs
@@ -16,5 +16,12 @@ namespace WorkWithGit
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form().Show();
+
+
+        }
     }
 }
